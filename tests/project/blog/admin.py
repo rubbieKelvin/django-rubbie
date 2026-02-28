2320108808edd8a7dd8ba7e690167f4b397c6cd8
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2026-02-28 19:24:11 +0100

    Updates.

--- tests/project/blog/admin.py
+++ tests/project/blog/admin.py
@@ -1,11 +1,15 @@
-from django.contrib import admin
+# from django.contrib import admin
+
+
+# @admin.register(Blog)
+# class BlogAdmin(admin.ModelAdmin):
+#     list_display = ("title", "slug", "published", "created_at")
+#     list_filter = ("published",)
+#     prepopulated_fields = {"slug": ("title",)}
+#     search_fields = ("title", "body")
+
+from rubbie.dashboard.model import register
 
 from .models import Blog
 
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "published", "created_at")
-    list_filter = ("published",)
-    prepopulated_fields = {"slug": ("title",)}
-    search_fields = ("title", "body")
+register(Blog)
